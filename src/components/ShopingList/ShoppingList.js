96cf963ec0b82f07a54dc2cd5158b8d83444e668
Nizar Sanchez <nizar4790k@gmail.com>
2021-07-05 17:42:08 -0400

    the shoping list is now working

--- src/components/ShopingList/ShoppingList.js
+++ src/components/ShopingList/ShoppingList.js
@@ -1,38 +1,69 @@
 
 import ShoppingListItem from "./ShoppingListItem";
 import NavBar from "../NavBar/NavBar";
-const ShoppingList = ({onSignOut, user})=>{
+import { useEffect, useState } from "react";
+const ShoppingList = ({ onSignOut, user }) => {
+
+  const [products, setProducts] = useState([])
 
 
-    return (
-      <div>
+  const fetchProductsCart = async () => {
+    const response = await fetch(process.env.REACT_APP_PROXY + `/cart/${user.id}`)
+    const result = await response.json();
+    setProducts(result.products);
+
+
+
+  }
+
+  useEffect(() => {
+
+    
+    fetchProductsCart();
+
+
+  }, [])
+
+
+  return (
+    <div>
       <NavBar onSignOut={onSignOut} fullName={user.fullName} selectedTab={"ShoppingList"}></NavBar>
       <div className="text-center container py-5">
 
-<h4 className="mt-4 mb-5"><strong>Lista de compra</strong></h4>
+        <h4 className="mt-4 mb-5"><strong>Lista de compra</strong></h4>
 
-<div className="d-flex justify-content-end">
-    
+        <form className="form">
+          <div className="form-row">
 
-    
-    
+            <div className="col-6">
+              <button className="btn btn-danger">Vaciar lista de compras</button>
+            </div>
 
-    <button className="btn btn-danger">Vaciar lista de compras</button>
-   
+          </div>
+
+
+        </form>
+
+
+
+        <section style={{ backgroundColor: "#eee" }}>
+          <div className="container py-5">
+
+            {
+              products.map((product, i) => {
+                console.log(product);
+                return <ShoppingListItem product={product} userId={user.id} key={i} fetchProductsCart={fetchProductsCart}></ShoppingListItem>
+              })
+            }
+
+
+
+          </div>
+        </section>
+      </div>
     </div>
 
-    <section style= {{backgroundColor: "#eee"}}>
-  <div className="container py-5">
-    
-      <ShoppingListItem ></ShoppingListItem>
-      <ShoppingListItem ></ShoppingListItem>
-    
-  </div>
-</section>
-     </div>  
-     </div>
-
-    );
+  );
 }
 
 export default ShoppingList;
--- src/components/ShopingList/ShoppingListItem.js
+++ src/components/ShopingList/ShoppingListItem.js
@@ -1,6 +1,31 @@
-const ShoppingListItem = ()=>{
-    return (
-        <div className="row justify-content-center mb-3">
+const ShoppingListItem = ({ product,userId,fetchProductsCart}) => {
+
+  const { img, name, description, hall, price, discount } = product;
+
+
+  const removeFromCart = async (userId, product) => {
+
+    console.log(userId);
+    console.log(product);
+
+    const response = await fetch(process.env.REACT_APP_PROXY + '/cart', {
+      method: 'put',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        product: product,
+        userId: userId
+      })
+    })
+    await fetchProductsCart();
+
+    
+
+
+
+  }
+
+  return (
+    <div className="row justify-content-center mb-3">
       <div className="col-md-12 col-xl-10">
         <div className="card shadow-0 border rounded-3">
           <div className="card-body">
@@ -8,61 +33,49 @@ const ShoppingListItem = ()=>{
               <div className="col-md-12 col-lg-3 col-xl-3 mb-4 mb-lg-0">
                 <div className="bg-image hover-zoom ripple rounded ripple-surface">
                   <img
-                    src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/img%20(4).jpg"
-                   className="w-100"
+                    src={img}
+                    className="w-100"
                   />
                   <a href="#!">
                     <div className="hover-overlay">
                       <div
-                       className="mask"
-                        style= {{backgroundColor: "rgba(253, 253, 253, 0.15)"}}
+                        className="mask"
+                        style={{ backgroundColor: "rgba(253, 253, 253, 0.15)" }}
                       ></div>
                     </div>
                   </a>
                 </div>
               </div>
               <div className="col-md-6 col-lg-6 col-xl-6">
-                <h5>Quant trident shirts</h5>
-                <div className="d-flex flex-row">
-                  <div className="text-danger mb-1 me-2">
-                    <i className="fa fa-star"></i>
-                    <i className="fa fa-star"></i>
-                    <i className="fa fa-star"></i>
-                    <i className="fa fa-star"></i>
-                  </div>
-                  <span>310</span>
-                </div>
-                    <div className="mt-1 mb-0 text-muted small">
-                  <span>100% cotton</span>
-                  <span className="text-primary"> • </span>
-                  <span>Light weight</span>
-                  <span className="text-primary"> • </span>
-                  <span>Best finish<br /></span>
-                </div>
-                <div className="mb-2 text-muted small">
-                  <span>Unique design</span>
-                  <span className="text-primary"> • </span>
-                  <span>For men</span>
-                  <span className="text-primary"> • </span>
-                  <span>Casual<br /></span>
-                </div>
-                <p className="text-truncate mb-4 mb-md-0">
-                  There are many variations of passages of Lorem Ipsum available, but the
-                  majority have suffered alteration in some form, by injected humour, or
-                  randomised words which don't look even slightly believable.
+                <h5>{name}</h5>
+
+
+                <p className="mb-4 mb-md-0 mt-4">
+                  {description}
                 </p>
+
+                <h6 className="mb-3 mt-4">
+            <strong>Pasillo</strong><b className="ml-1 text-danger">{hall}</b>
+           </h6>
+
+
               </div>
               <div className="col-md-6 col-lg-3 col-xl-3 border-sm-start-none border-start">
                 <div className="d-flex flex-row align-items-center mb-1">
-                  <h4 className="mb-1 me-1">$13.99</h4>
-                  <span className="text-danger"><s>$20.99</s></span>
+                  <h4 className="mb-1 me-1">${price * (1 - discount)}</h4>
+                  <span className="text-danger ml-3"><s>${price}</s></span>
                 </div>
-                <h6 className="text-success">Free shipping</h6>
+
                 <div className="d-flex flex-column mt-4">
-                  <button className="btn btn-primary btn-sm" type="button">Details</button>
-                  <button className="btn btn-outline-primary btn-sm mt-2" type="button">
-                    Add to wishlist
-                  </button>
+                  <button className="btn btn-outline-danger btn-sm mb-4" type="button" onClick={()=>removeFromCart(userId,product)}>Borrar de la lista</button>
+                  <div className="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+                    <label class="form-check-label" for="flexCheckDefault">
+                      Marcar como comprado
+                    </label>
+
+                  </div>
+
                 </div>
               </div>
             </div>
@@ -70,7 +83,7 @@ const ShoppingListItem = ()=>{
         </div>
       </div>
     </div>
-    )
+  )
 }
 
 export default ShoppingListItem;
